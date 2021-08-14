c370184237a93273f9ec85800a3865c1fc766e1d
nickolayuhorcev <nickolayuhorcev@yandex.ru>
2021-08-14 09:14:42 +0300

    first commit

---
+++ HomeWork-1.js
@@ -0,0 +1,23 @@
+// ДЗ-1. Задание 1.
+
+let tempCels = parseInt("100Grad");
+let tempForeng = (9 / 5) * tempCels + 32;
+console.log(tempForeng);
+
+
+// ДЗ-1. Задание 2.
+//Имя переменной name в VS Code перечеркнуто, поэтому использовал другое имя
+
+
+let userName = 'Василий';
+let admin;
+admin = userName;
+console.log(admin);
+
+// ДЗ-1. Задание 3.
+
+//Чему будет равно JS-выражение 1000 + "108"?
+
+//Ответ: 1000108, т.к. происходит конкатенация числа и строки.  
+
+console.log(1000 + '108');
